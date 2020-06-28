5755f681c8e1ceb7962b3023492d3797d2a44d12
foxten <tenaysiafox@gmail.com>
2020-06-27 22:43:32 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(bit_of_knowledge)
+        @knowledge << bit_of_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
